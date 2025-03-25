1036c439200d534ae633f588deaf0cab18896695
Tigran <tigra.azaryan@me.com>
2025-03-24 17:42:16 -0700

    Updated test_login.py and requirements.txt files Added .gitignore file

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,40 +1,32 @@
 import pytest
+import allure
 
 
+@allure.feature('Авторизация')
+@allure.story('Авторизация с недействительные учетные данные')
+@allure.severity(allure.severity_level.CRITICAL)
+@allure.title('Авторизация с недействительными учетными данными')
 def test_login_failure(login_page):
-    login_page.navigate()
-    login_page.login('user', 'password')
+    with allure.step('Открыть страницу авторизации'):
+        login_page.navigate()
+    with allure.step('Ввести в форму авторизации недействительные учетные данные'):
+        login_page.login('invalid_user', 'invalid_password')
+    with allure.step('Отображается ошибка - Invalid credentials. Please try again.'):
+        assert login_page.get_error_message() == 'Invalid credentials. Please try again.'
 
-    assert login_page.get_error_message() == 'Invalid credentials. Please try again.'
 
-
+@allure.feature('Login')
+@allure.story('Login with valid credentials')
+@allure.severity(allure.severity_level.CRITICAL)
+@allure.title('Авторизация с корректными учетными данными')
 @pytest.mark.parametrize('username, password', [
     ('user', 'user'),
     ('admin', 'admin')
 ])
 def test_login_success(login_page, dashboard_page, username, password):
-    login_page.navigate()
-    login_page.login(username,password)
-    dashboard_page.assert_welcome_message(f"Welcome {username}")
-
-'''
-from pages.login_page import LoginPage
-from pages.dashboard_page import DashboardPage
-
-
-def test_login_failure(page):
-    login_page = LoginPage(page)
-    login_page.navigate()
-    login_page.login('invalid_user', 'invalid_password')
-    assert login_page.get_error_message() == 'Invalid credentials. Please try again.'
-
-
-def test_login_success(page):
-    login_page = LoginPage(page)
-    dashboard_page = DashboardPage(page)
-
-    login_page.navigate()
-    login_page.login('admin', 'admin')
-
-    dashboard_page.assert_welcome_message("Welcome admin")
-'''
+    with allure.step('Открыть страницу авторизации'):
+        login_page.navigate()
+    with allure.step('Ввести в форму авторизации недействительные учетные данные'):
+        login_page.login(username, password)
+    with allure.step('Отображается приветственное сообщение с именем пользователя'):
+        dashboard_page.assert_welcome_message(f"Welcome {username}")
